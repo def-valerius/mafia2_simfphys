f526b7e7d208da5dc18930971d1d28490ad3417b
def-valerius <31399259+def-valerius@users.noreply.github.com>
2017-12-07 15:41:52 +0500

    sounds 'n lua

--- lua/autorun/simfphys_mafia2_40s_spawnlist.lua
+++ lua/autorun/simfphys_mafia2_40s_spawnlist.lua
@@ -1,112 +1,150 @@
 local V = {
-	Name = "Benson",
-	Model = "models/GTA_SA/industrial/benson.mdl",
+	Name = "Culver Empire",
+	Model = "models/mafia2/ulver_newyorker.mdl",
 	Class = "gmod_sent_vehicle_fphysics_base",
-	Category = "GTA SA Industrial",
+	Category = "Mafia 2 40s",
 	SpawnOffset = Vector(0,0,20),
 	SpawnAngleOffset = 90,
 
 	Members = {
-		Mass = 3500,
+		Mass = 1750,
 		
-		EnginePos = Vector(60,0,10),
+		EnginePos = Vector(60,0,20),
 		
-		LightsTable = "benson",
+		LightsTable = "ulver_newyorker",
 		
 		CustomWheels = true,
-		CustomSuspensionTravel = 5,
+		CustomSuspensionTravel = 1.5,
+				CustomWheelModel = "models/mafia2/dunniel_black_rook.mdl",
+		CustomWheelPosFL = Vector(64,32,0),
+		CustomWheelPosFR = Vector(64,-32,0),
+		CustomWheelPosRL = Vector(-59,32,2),
+		CustomWheelPosRR = Vector(-59,-32,2),
+		CustomWheelAngleOffset = Angle(0,270,0),
 		
-		CustomWheelModel = "models/GTA_SA/industrial/benson_wheel.mdl",
-		CustomWheelPosFL = Vector(62,31,-24),
-		CustomWheelPosFR = Vector(62,-31,-24),
-		CustomWheelPosRL = Vector(-77,33,-24),
-		CustomWheelPosRR = Vector(-77,-33,-24),
-		CustomWheelAngleOffset = Angle(0,90,0),
-		
-		CustomMassCenter = Vector(10,0,0),
+		CustomMassCenter = Vector(10,0,5),
 		
 		CustomSteerAngle = 30,
 		
-		SeatOffset = Vector(5,-16,17),
-		SeatPitch = 2,
+		SeatOffset = Vector(-1,-13,40),
+		SeatPitch = -4,
 		SeatYaw = 90,
 		
 		PassengerSeats = {
 			{
-				pos = Vector(12,-17,-12),
-				ang = Angle(0,-90,8)
+				pos = Vector(7,-15,8),
+				ang = Angle(0,-90,17)
+			},
+			{
+				pos = Vector(-35,-15,5),
+				ang = Angle(0,-90,17)
+			},
+			{
+				pos = Vector(-35,15,5),
+				ang = Angle(0,-90,17)
 			}
 		
 		},
 		
 		ExhaustPositions = {
+						{
+				pos = Vector(-98,12,2),
+				ang = Angle(90,165,0),
+				OnBodyGroups = { 
+					[7] = {0},
+				}
+			},
 			{
-				pos = Vector(-120,-17,-25),
-				ang = Angle(90,180,0),
+				pos = Vector(-30,-36,0),
+				ang = Angle(90,-125,0),
+				OnBodyGroups = { 
+					[7] = {1},
+				}
+			},
+			{
+				pos = Vector(-33,-36,0),
+				ang = Angle(90,-125,0),
+				OnBodyGroups = { 
+					[7] = {1},
+				}
+			},
+			{
+				pos = Vector(-30,36,0),
+				ang = Angle(90,125,0),
+				OnBodyGroups = { 
+					[7] = {1},
+				}
+			},
+			{
+				pos = Vector(-33,36,0),
+				ang = Angle(90,125,0),
+				OnBodyGroups = { 
+					[7] = {1},
+				}
 			}
 		},
 		
-		FrontHeight = 6,
-		FrontConstant = 50000,
-		FrontDamping = 3000,
-		FrontRelativeDamping = 3000,
+		FrontHeight = 8,
+		FrontConstant = 24000,
+		FrontDamping = 1000,
+		FrontRelativeDamping = 800,
 		
 		RearHeight = 8,
-		RearConstant = 50000,
-		RearDamping = 3000,
-		RearRelativeDamping = 3000,
+		RearConstant = 24000,
+		RearDamping = 1000,
+		RearRelativeDamping = 800,
 		
-		FastSteeringAngle = 18,
+		FastSteeringAngle = 30,
 		SteeringFadeFastSpeed = 535,
 		
-		TurnSpeed = 2,
+		TurnSpeed = 1.2,
 		
-		MaxGrip = 50,
+		MaxGrip = 42,
 		Efficiency = 1,
-		GripOffset = -5,
-		BrakePower = 50,
+		GripOffset = 1.5,
+		BrakePower = 22,
 		
-		IdleRPM = 600,
-		LimitRPM = 4000,
-		PeakTorque = 280,
-		PowerbandStart = 700,
-		PowerbandEnd = 3800,
+		IdleRPM = 700,
+		LimitRPM = 4500,
+		PeakTorque = 130,
+		PowerbandStart = 900,
+		PowerbandEnd = 4300,
 		Turbocharged = false,
 		Supercharged = false,
 		
-		FuelFillPos = Vector(-40,37,-13),
-		FuelType = FUELTYPE_DIESEL,
-		FuelTankSize = 60,
+		FuelFillPos = Vector(-75,34,26),
+		FuelType = FUELTYPE_PETROL,
+		FuelTankSize = 70,
 		
 		PowerBias = 1,
 		
 		EngineSoundPreset = 0,
 
 		
-		Sound_Idle = "bank_077/sound_002.wav",
+		Sound_Idle = "mafia2/V8_40_normal2_0.wav",
 		Sound_IdlePitch = 1,
 		
-		Sound_Mid = "bank_077/sound_001.wav",
-		Sound_MidPitch = 1.5,
+		Sound_Mid = "mafia2/V8_40_normal2_1b.wav",
+		Sound_MidPitch = 1.2,
 		Sound_MidVolume = 2,
 		Sound_MidFadeOutRPMpercent = 80,
-		Sound_MidFadeOutRate = 0.8,
+		Sound_MidFadeOutRate = 0.5,
 		
-		Sound_High = "bank_077/sound_001.wav",
-		Sound_HighPitch = 1.1,
+		Sound_High = "mafia2/V8_40_normal2_2.wav",
+		Sound_HighPitch = 1.2,
 		Sound_HighVolume = 1.5,
 		Sound_HighFadeInRPMpercent = 80,
-		Sound_HighFadeInRate = 0.8,
+		Sound_HighFadeInRate = 0.5,
 		
-		Sound_Throttle = "bank_078/sound_002.wav",
-		Sound_ThrottlePitch = 1,
-		Sound_ThrottleVolume = 4,
+		Sound_Throttle = "mafia2/V8_40_normal2_3.wav",
+		Sound_ThrottlePitch = 1.2,
+		Sound_ThrottleVolume = 1,
 		
 		--
-		snd_horn = "bank_068/sound_002.wav",
+		snd_horn = "mafia2/A_klax_Ulver_l.wav",
 		
-		DifferentialGear = 0.5,
-		Gears = {-0.12,0,0.14,0.24,0.36,0.48,0.6}
+		DifferentialGear = 0.55,
+		Gears = {-0.12,0,0.1,0.25,0.44,}
 	}
 }
-list.Set( "simfphys_vehicles", "simfphys_gta_sa_benson", V )
+list.Set( "simfphys_vehicles", "simfphys_mafia2_ulver_newyorker", V )
