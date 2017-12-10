6d591c860e309eaa0f76b2ccad594d3c6697ab61
def-valerius <31399259+def-valerius@users.noreply.github.com>
2017-12-10 18:58:12 +0500

    ent of Houston Wasp

--- lua/autorun/simfphys_mafia2_50s_ligths.lua
+++ lua/autorun/simfphys_mafia2_50s_ligths.lua
@@ -97,3 +97,52 @@ local light_table = {
 
 }
 list.Set( "simfphys_lights", "bkingf", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 98, 32, 26 ),
+	L_HeadLampAng = Angle(5,0,0),
+	R_HeadLampPos = Vector( 98, -32, 26 ),
+	R_HeadLampAng = Angle(5,0,0),
+	
+	L_RearLampPos = Vector(-101,31,24),
+	L_RearLampAng = Angle(10,180,0),
+	R_RearLampPos = Vector(-101,-31,24),
+	R_RearLampAng = Angle(10,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 98, 32, 26 ),material = "sprites/light_ignorez",size = 42, color = Color( 255,220,200,230)},
+		{pos = Vector( 98, -32, 26 ),material = "sprites/light_ignorez",size = 42, color = Color( 255,220,200,230)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 98, 32, 26 ),material = "sprites/light_ignorez",size = 45, color = Color( 255,235,220,240)},
+		{pos = Vector( 98, -32, 26 ),material = "sprites/light_ignorez",size = 45, color = Color( 255,235,220,240)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-101,31,24),material = "sprites/light_ignorez",size = 23,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-101,-31,24),material = "sprites/light_ignorez",size = 23,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-101,31,22),material = "sprites/light_ignorez",size = 25,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-101,-31,22),material = "sprites/light_ignorez",size = 25,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-100,31,28),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-100,-31,28),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-100,31,26),
+			Vector( 94, 34, 12 ),
+		},
+		Right = {
+			Vector(-100,-31,26),
+			Vector( 94, -34, 12 ),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "Hwasp", light_table)
--- lua/autorun/simfphys_mafia2_50s_spawnlist.lua
+++ lua/autorun/simfphys_mafia2_50s_spawnlist.lua
@@ -263,3 +263,155 @@ local V = {
 	}
 }
 list.Set( "simfphys_vehicles", "simfphys_mafia2_bkingf", V )
+
+local V = {
+	Name = "Houston Wasp",
+	Model = "models/mafia2/Hwasp.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "Mafia 2 50s",
+	SpawnOffset = Vector(0,0,20),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 1636,
+		
+		EnginePos = Vector(75,0,20),
+		
+		LightsTable = "Hwasp",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 1.5,
+		
+		CustomWheelModel = "models/mafia2/dunniel_black_rook.mdl",
+		CustomWheelPosFL = Vector(70,32,2),
+		CustomWheelPosFR = Vector(70,-32,2),
+		CustomWheelPosRL = Vector(-58,32,2),
+		CustomWheelPosRR = Vector(-58,-32,2),
+		CustomWheelAngleOffset = Angle(0,270,0),
+		
+		CustomMassCenter = Vector(10,0,5),
+		
+		CustomSteerAngle = 30,
+		
+		SeatOffset = Vector(6,-16,39),
+		SeatPitch = -4,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(12,-15,8),
+				ang = Angle(0,-90,17)
+			},
+			{
+				pos = Vector(-29,-15,5),
+				ang = Angle(0,-90,17)
+			},
+			{
+				pos = Vector(-29,15,5),
+				ang = Angle(0,-90,17)
+			}
+		
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-108,12,0),
+				ang = Angle(90,165,0),
+				OnBodyGroups = { 
+					[7] = {0},
+				}
+			},
+			{
+				pos = Vector(-29,-38,-2),
+				ang = Angle(90,-125,0),
+				OnBodyGroups = { 
+					[7] = {1},
+				}
+			},
+			{
+				pos = Vector(-32,-38,-2),
+				ang = Angle(90,-125,0),
+				OnBodyGroups = { 
+					[7] = {1},
+				}
+			},
+			{
+				pos = Vector(-29,38,-2),
+				ang = Angle(90,125,0),
+				OnBodyGroups = { 
+					[7] = {1},
+				}
+			},
+			{
+				pos = Vector(-32,38,-2),
+				ang = Angle(90,125,0),
+				OnBodyGroups = { 
+					[7] = {1},
+				}
+			}
+		},
+		
+		FrontHeight = 8,
+		FrontConstant = 24000,
+		FrontDamping = 1000,
+		FrontRelativeDamping = 800,
+		
+		RearHeight = 8,
+		RearConstant = 24000,
+		RearDamping = 1000,
+		RearRelativeDamping = 800,
+		
+		FastSteeringAngle = 30,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 1.2,
+		
+		MaxGrip = 42,
+		Efficiency = 1,
+		GripOffset = 1.5,
+		BrakePower = 25,
+		
+		IdleRPM = 700,
+		LimitRPM = 5000,
+		PeakTorque = 150,
+		PowerbandStart = 900,
+		PowerbandEnd = 4700,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-75,35,30),
+		FuelType = FUELTYPE_PETROL,
+		FuelTankSize = 70,
+		
+		PowerBias = 1,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "mafia2/V8_normal3_0.wav",
+		Sound_IdlePitch = 1,
+		
+		Sound_Mid = "mafia2/V8_normal3_1.wav",
+		Sound_MidPitch = 1,
+		Sound_MidVolume = 2,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.5,
+		
+		Sound_High = "mafia2/V8_normal3_2.wav",
+		Sound_HighPitch = 1,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.5,
+		
+		Sound_Throttle = "mafia2/V8_normal3_3.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 1,
+		
+		--
+		snd_horn = "mafia2/A_klax_Houston_l.wav",
+		
+		DifferentialGear = 0.5,
+		Gears = {-0.12,0,0.1,0.26,0.52}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_mafia2_Hwasp", V )
