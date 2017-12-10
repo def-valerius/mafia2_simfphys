0a416eb7db2a24a58dce02db4192de46ff530f6a
def-valerius <31399259+def-valerius@users.noreply.github.com>
2017-12-10 19:38:17 +0500

    ent of ISW 508

--- lua/autorun/simfphys_mafia2_50s_spawnlist.lua
+++ lua/autorun/simfphys_mafia2_50s_spawnlist.lua
@@ -415,3 +415,146 @@ local V = {
 	}
 }
 list.Set( "simfphys_vehicles", "simfphys_mafia2_Hwasp", V )
+
+local V = {
+	Name = "ISW 508",
+	Model = "models/mafia2/isw_508.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "Mafia 2 50s",
+	SpawnOffset = Vector(0,0,20),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 1315,
+		
+		EnginePos = Vector(60,0,20),
+		
+		LightsTable = "isw_508",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 1.5,
+		
+		CustomWheelModel = "models/mafia2/galahad_silver_band.mdl",
+		CustomWheelPosFL = Vector(67,30,6),
+		CustomWheelPosFR = Vector(67,-30,6),
+		CustomWheelPosRL = Vector(-42,30,8),
+		CustomWheelPosRR = Vector(-42,-30,8),
+		CustomWheelAngleOffset = Angle(0,270,0),
+		
+		CustomMassCenter = Vector(10,0,5),
+		
+		CustomSteerAngle = 30,
+		
+		SeatOffset = Vector(-14,-15,36),
+		SeatPitch = -9,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(-4,-15,3),
+				ang = Angle(0,-90,25)
+			}
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-83,-12,4),
+				ang = Angle(90,165,0),
+				OnBodyGroups = { 
+					[7] = {0},
+				}
+			},
+			{
+				pos = Vector(-14,-33,-1),
+				ang = Angle(90,-125,0),
+				OnBodyGroups = { 
+					[7] = {1},
+				}
+			},
+			{
+				pos = Vector(-17,-33,-1),
+				ang = Angle(90,-125,0),
+				OnBodyGroups = { 
+					[7] = {1},
+				}
+			},
+			{
+				pos = Vector(-14,33,-1),
+				ang = Angle(90,125,0),
+				OnBodyGroups = { 
+					[7] = {1},
+				}
+			},
+			{
+				pos = Vector(-17,33,-1),
+				ang = Angle(90,125,0),
+				OnBodyGroups = { 
+					[7] = {1},
+				}
+			}
+		},
+		
+		FrontHeight = 8,
+		FrontConstant = 24000,
+		FrontDamping = 1000,
+		FrontRelativeDamping = 800,
+		
+		RearHeight = 8,
+		RearConstant = 24000,
+		RearDamping = 1000,
+		RearRelativeDamping = 800,
+		
+		FastSteeringAngle = 30,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 1.2,
+		
+		MaxGrip = 46,
+		Efficiency = 1,
+		GripOffset = 1.5,
+		BrakePower = 26,
+		
+		IdleRPM = 700,
+		LimitRPM = 5400,
+		PeakTorque = 160,
+		PowerbandStart = 900,
+		PowerbandEnd = 5200,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-52,35,27),
+		FuelType = FUELTYPE_PETROL,
+		FuelTankSize = 70,
+		
+		PowerBias = 1,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "mafia2/V8_sport3_1.wav",
+		Sound_IdlePitch = 1,
+		
+		Sound_Mid = "mafia2/V8_sport3_2.wav",
+		Sound_MidPitch = 1,
+		Sound_MidVolume = 2,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.5,
+		
+		Sound_High = "mafia2/V8_sport3_3.wav",
+		Sound_HighPitch = 1,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.5,
+		
+		Sound_Throttle = "mafia2/V8_sport3_4.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 1,
+		
+		--
+		snd_horn = "mafia2/A_klax_ISW_l.wav",
+		
+		DifferentialGear = 0.5,
+		Gears = {-0.12,0,0.1,0.22,0.37,0.57}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_mafia2_isw_508", V )
